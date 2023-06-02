30f3cb5e3fab70c737517a7b265be002b8b446cb
dhmm <dhmm@outlook.com>
2023-06-02 23:45:50 +0300

    comments service implemented

---
+++ blog/comments/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments' , (req,res) => { 
+	
+	//if the array item is undefined send an empty array	
+	res.status(201).send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments' , (req,res) => { 
+	const commentId = randomBytes(4).toString('hex');
+	const { content } = req.body;
+
+	//if the array item is undefined give as an empty array
+	const comments = commentsByPostId[req.params.id] || [];
+	
+	comments.push( {id:commentId ,  content});
+	commentsByPostId[req.params.id] = comments;
+
+	res.status(201).send(comments);
+});
+
+
+app.listen(4001, ()=> {
+	console.log('Running on 4001');
+})
