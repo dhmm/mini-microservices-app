764413f3d8127c966eeab457359b52557d986de0
dhmm <dhmm@outlook.com>
2023-06-12 14:15:08 +0300

    comments list done

---
+++ blog/client/components/CommentList.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      comments: []
+    }
+  },
+  props: [ 'postId' ],	
+  methods: {
+    async getData() {
+      const res = await fetch("http://localhost:4001/posts/"+this.postId+"/comments");
+      const finalRes = await res.json();
+      this.comments = finalRes;
+    }
+  },
+  mounted() {
+    this.getData()
+  }	
+}
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="comment in this.comments">{{comment.content}}</li>
+    </ul>
+  </div>
+</template>
--- blog/client/components/PostList.vue
+++ blog/client/components/PostList.vue
@@ -25,6 +25,7 @@
  		<ul>
  			<li style="width:300px;border:1px solid #000; background:#ccc;float:left;margin:5px;padding:5px;list-style:none;" v-for="post in this.posts">
         <h3>{{ post.title }}</h3>
+        <CommentList :postId="post.id"/>
         <CommentCreate :postId="post.id"/>
       </li>
  		</ul>
