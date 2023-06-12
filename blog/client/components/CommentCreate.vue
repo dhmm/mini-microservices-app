0419334694170d7610bc040b28f0cb86bdddcc64
dhmm <dhmm@outlook.com>
2023-06-12 14:01:02 +0300

    comment creation done

---
+++ blog/client/components/CommentCreate.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from "axios";
+export default {
+  	data() {
+		return {
+			content: ''
+		}
+	},
+  props: [ 'postId' ],	
+	methods: {
+		createComment() {      
+			axios.post('http://localhost:4001/posts/'+this.postId+'/comments', {
+				headers: {
+					'Content-Type': 'application/json',
+          'id' : this.postId
+				},
+        
+				content: this.content
+			})
+			.then(function (response) {
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+			
+		}
+	}
+}
+</script>
+
+<template>
+  <div>
+    <label for="content">Comment : </label><br>
+    <input type="text" v-model="content" name="content"/><br>
+    <input @click="createComment" type="button" value="Add comment"/>
+  </div>
+</template>
--- blog/client/components/PostList.vue
+++ blog/client/components/PostList.vue
@@ -23,7 +23,10 @@
  	<div>
  		Post List 
  		<ul>
- 			<li v-for="post in this.posts">{{ post.title }}</li>
+ 			<li style="width:300px;border:1px solid #000; background:#ccc;float:left;margin:5px;padding:5px;list-style:none;" v-for="post in this.posts">
+        <h3>{{ post.title }}</h3>
+        <CommentCreate :postId="post.id"/>
+      </li>
  		</ul>
  	</div>
 </template>
